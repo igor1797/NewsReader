016efcf3cce307d6396f991eec025dcc8ea34c3e
igor1797 <48154245+igor1797@users.noreply.github.com>
2020-11-14 00:42:23 +0100

    Added DataBindingAdapter.

---
+++ app/src/main/java/igor/kuridza/dice/newsreader/ui/adapters/DataBindingAdapter.kt
@@ -0,0 +1,19 @@
+package igor.kuridza.dice.newsreader.ui.adapters
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import igor.kuridza.dice.newsreader.common.loadImage
+
+object DataBindingAdapter {
+
+    @BindingAdapter("app:imageUrl")
+    @JvmStatic fun setImageUrl(imageView: ImageView, imageUrl: String){
+        imageView.loadImage(imageUrl)
+    }
+
+    @BindingAdapter("app:setAdapter")
+    @JvmStatic fun setRecyclerViewAdapter(recyclerView: RecyclerView, adapter: RecyclerView.Adapter<*>){
+            recyclerView.adapter = adapter
+    }
+}
