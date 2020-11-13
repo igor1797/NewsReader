23824bb469a7b5c6cc2ea5e8e59789384dbf4b75
igor1797 <48154245+igor1797@users.noreply.github.com>
2020-11-13 22:18:13 +0100

    Created adapter, holder and layout for recycler view to show list of news. Created NewsResponse models and implemented Retrofit and Fragment Navigation Component

--- app/src/main/java/igor/kuridza/dice/newsreader/ui/activities/MainActivity.kt
+++ app/src/main/java/igor/kuridza/dice/newsreader/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package igor.kuridza.dice.newsreader
+package igor.kuridza.dice.newsreader.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import igor.kuridza.dice.newsreader.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
